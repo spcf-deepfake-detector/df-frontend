de5ee4127b168c06e57564006baa99f5afad414d
Gabriel Gatbonton <gabriel.gatbonton3262@gmail.com>
2024-11-25 17:27:58 +0800

    Added Packages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+// Mantine
+import { MantineProvider } from "@mantine/core";
+import theme from "./plugins/mantine.js";
+import "@mantine/core/styles.css";
+
+// React Router
+import { createBrowserRouter } from "react-router";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <MantineProvider theme={theme}>
+      
+    </MantineProvider>
+  </StrictMode>
+);
